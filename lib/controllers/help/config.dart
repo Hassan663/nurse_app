c1829858717ee6e3f65d2d764d685a89f6cb01c9
hamxa152 <33662538+hamxa152@users.noreply.github.com>
2021-11-08 02:29:01 -0800

    agora added

---
+++ lib/controllers/help/config.dart
@@ -0,0 +1,14 @@
+/// Get your own App ID at https://dashboard.agora.io/
+const String appId = '9cfc9bdb58de436d8068ce28e2505811';
+
+/// Please refer to https://docs.agora.io/en/Agora%20Platform/token
+const String token = '0069cfc9bdb58de436d8068ce28e2505811IACbaJLBgQLqw3Qiqwe2lIjErg8rOoiVBOblODh9QEwrdRgwvN8AAAAAEAD3dRUDFwuKYQEAAQAXC4ph';
+
+/// Your channel ID
+const String channelId = 'checkapp';
+
+/// Your int user ID
+const int uid = 1;
+
+/// Your string user ID
+const String stringUid = '1';
