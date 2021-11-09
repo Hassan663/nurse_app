858a68444fc2953c3e988eec8ef91a257222061e
Hassan663 <hn893385@gmail.com>
2021-11-09 21:42:03 +0500

    Token is updated

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: HomePage(),
+        home: AuthDecider(),
       ),
       designSize: const Size(1920, 1080),
     );
