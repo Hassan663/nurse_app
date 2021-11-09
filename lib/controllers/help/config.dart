d7e8f1e6601d90b4c5ba522a9c2045b3d6595363
Hassan663 <hn893385@gmail.com>
2021-11-09 21:41:54 +0500

    Token is updated

--- lib/controllers/help/config.dart
+++ lib/controllers/help/config.dart
@@ -1,11 +1,12 @@
 /// Get your own App ID at https://dashboard.agora.io/
-const String appId = '9cfc9bdb58de436d8068ce28e2505811';
+const String appId = '8dee1e4410c84e06bbf3226327db5a86';
 
 /// Please refer to https://docs.agora.io/en/Agora%20Platform/token
-const String token = '0069cfc9bdb58de436d8068ce28e2505811IADoIw0jiFXWVBoXVU4co4+1TK5jTFhlzaWDDxO6YDqj0ssskgoAAAAAEAD3dRUDj1GLYQEAAQCPUYth';
+const String token =
+    '0068dee1e4410c84e06bbf3226327db5a86IABYyTZiqOmyry+bfMX/BkZowWpBiYDpSOALcuAHsokAvtJjSIgAAAAAEACkCrtyUuSLYQEAAQBD5Ith';
 
 /// Your channel ID
-const String channelId = 'callinguser';
+const String channelId = '123';
 
 /// Your int user ID
 const int uid = 1;
