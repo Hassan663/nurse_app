0208ff193bc11c225ddce58e79ed91fe4efd6157
hamxa152 <33662538+hamxa152@users.noreply.github.com>
2021-11-10 07:54:48 -0800

    calling done

--- lib/controllers/help/config.dart
+++ lib/controllers/help/config.dart
@@ -3,7 +3,7 @@ const String appId = '8dee1e4410c84e06bbf3226327db5a86';
 
 
 const String token =
-    '0068dee1e4410c84e06bbf3226327db5a86IABYyTZiqOmyry+bfMX/BkZowWpBiYDpSOALcuAHsokAvtJjSIgAAAAAEACkCrtyUuSLYQEAAQBD5Ith';
+    '0068dee1e4410c84e06bbf3226327db5a86IABfwY7ILmct8huIoIxxEk5iCx01HgQh5SoGEMmXbPRtiNJjSIgAAAAAEACkCrtyrjeNYQEAAQCeN41h';
 
 
 const String channelId = '123';
